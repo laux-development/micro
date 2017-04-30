4eac7d60a121205f71c55e030e8ae76b8280e159
James Laux <jamie@bleeppurple.com>
2017-04-30 13:38:02 +0100

    removed tmpl from main go app

--- micro.go
+++ micro.go
@@ -1,20 +1,8 @@
 package micro
 
 import (
-	"html/template"
 	"net/http"
 )
-
-// templates
-// parse and cache templates
-var templates = template.Must(template.ParseFiles(
-	"templates/components/login/login-element.tmpl",
-	"templates/components/login/login-cdn-library.tmpl",
-	"templates/components/login/login-script.tmpl",
-	"templates/components/login/login-return-script.tmpl",
-	"pages/login.tmpl",
-	"pages/home.tmpl",
-))
 
 func init() {
 	http.HandleFunc("/", home)
